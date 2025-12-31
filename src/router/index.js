5dfe7a085a9b4f781642220e7bb0d8d14faade91
Miles <wangle2071@163.com>
2025-12-31 15:12:32 +0800

    调整config配置，适配niginx部署

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const Home = () => import('@/views/Home/Home.vue')
 const About = () => import('@/views/About/about.vue')
@@ -28,7 +28,7 @@ const routes = [{
   }]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory('/mymobile/'),
   routes,
 })
 
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/mymobile/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
